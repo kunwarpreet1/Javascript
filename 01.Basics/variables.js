79464638fcf373be393e91315f2bb21da7ea18c1
kunwarpreet1 <111242792+kunwarpreet1@users.noreply.github.com>
2023-10-03 08:19:38 +0000

    config done

---
+++ 01.Basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12234//value cannot be changed
+let accountEmail = "kunwar@google.com"//mostly used block scope
+var city = "Mumbai"//not mostly used because global scope
+accountNottoUse = "Donot use this type of Practise of Assigning Variables"//not to use generally
+// console.log(accountId);
+
+accountEmail = "Kunwar@yahoo.com"
+city = "Delhi"
+
+
+console.table([accountId,accountEmail,city,accountNottoUse])
