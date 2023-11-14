cbec269d3da365db25fbb99d2a18d10c02dcd523
kunwarpreet1 <111242792+kunwarpreet1@users.noreply.github.com>
2023-11-14 07:37:48 +0000

    datatypes

---
+++ 01.Basics/datatypes.js
@@ -0,0 +1,7 @@
+"use strict" //make for letting all the js code in new version of JavaScript
+
+let age = 10;
+let name = "kunwar";
+
+alert("kunwar");//not use in code editor
+console.log(typeof age);
